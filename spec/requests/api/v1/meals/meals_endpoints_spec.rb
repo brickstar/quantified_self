612df7f5dc8c0e4df2494535cdeeec20ef290631
brickstar <brickstar@gmail.com>
2018-10-06 19:16:22 -0600

    Add meals endpoint spec, fix meals factory to include foods

--- spec/requests/api/v1/meals/meals_endpoints_spec.rb
+++ spec/requests/api/v1/meals/meals_endpoints_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 describe "Meals API" do
+  before(:each) do
+    @meals = create_list(:meal, 3, :with_foods)
+  end
+
   it "sends a list of meals" do
-    meals = create_list(:meal, 3)
-binding.pry
     get '/api/v1/meals'
 
     expect(response).to be_successful
@@ -12,7 +14,9 @@ binding.pry
     meal = meals.first
 
     expect(meals.count).to eq(3)
-    expect(meal).to have_key(:name)
+    expect(meals).to have_key(:name)
+    expect(meals).to have_key(:foods)
+    expect(meals).to have_key
     expect(meal).to_not have_key(:created_at)
     expect(meal).to_not have_key(:updated_at)
   end

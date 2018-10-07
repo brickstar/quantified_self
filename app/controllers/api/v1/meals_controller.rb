750f2e315f7d49f0f76eeeb667625290d4f4c1f9
brickstar <brickstar@gmail.com>
2018-10-06 20:22:14 -0600

    Add create action and sad path

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -11,4 +11,15 @@ class Api::V1::MealsController < ApplicationController
       render status: 404
     end
   end
+
+  def create
+    meal = Meal.find_by(id: params[:meal_id])
+    food = Food.find_by(id: params[:id])
+    if food && meal
+      MealFood.create(meal_id: params[:meal_id], food_id: params[:id])
+      render json: { "message": "Successfully added #{food.name} to #{meal.name}" }, status: 201
+    else
+      render status: 404
+    end
+  end
 end

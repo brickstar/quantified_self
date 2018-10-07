84ee7b3fdc3274755a1376b4d4a1ca7b8bbafe25
brickstar <brickstar@gmail.com>
2018-10-06 19:54:28 -0600

    Add meal show action

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -2,4 +2,13 @@ class Api::V1::MealsController < ApplicationController
   def index
     render json: Meal.all
   end
+
+  def show
+    meal = Meal.find_by(id: params[:meal_id])
+    if meal
+      render json: meal
+    else
+      render status: 404
+    end
+  end
 end

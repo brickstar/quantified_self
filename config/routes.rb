f362ec78f49ab357e1dc1908bb696c7f675ad472
brickstar <brickstar@gmail.com>
2018-10-06 19:54:57 -0600

    Add route for meal/id/foods

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :foods
-      resources :meals
+      resources :meals, only: [:index, :show] do
+        resources :foods, only: [:index], to: "meals#show"
+      end
     end
   end
 end

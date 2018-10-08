119de75f1175db9351d5eae7aca8848038f548b2
brickstar <brickstar@gmail.com>
2018-10-07 20:43:52 -0600

    Add route for favorite foods endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/favorite_foods', to: "favorite_foods#index"
       resources :foods
       resources :meals, only: [:index, :show] do
         get '/foods', to: "meals#show"
